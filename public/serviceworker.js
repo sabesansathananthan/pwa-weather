0df892b0f7e867fc4ce5126b7e6b66a66eebc594
sabesan-satha <sabesan.sathananthan@syscolabs.com>
2022-07-21 23:47:46 +0530

    feat: pwa

---
+++ public/serviceworker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "v1";
+const cacheURL = ["index.html", "offline.html"];
+
+const self = this;
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+
+      return cache.addAll(cacheURL);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() => caches.match("offline.html"));
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [];
+  cacheWhitelist.push(CACHE_NAME);
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
